00f332891ca69b74049c2b2e0f1adeaa9264ac2f
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-05 19:39:48 -0800

    Time: 106 ms (15.01%), Space: 19.3 MB (11.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> substrings;
-
-        for (int i = 0; i < s.length(); i++) {
-            string total_string = "";
-
-            for (int j = 0; j < s.length() - i; j++) {
-                if (total_string.find(s[i + j]) != string::npos) {
-                    break;
-                }
-
-                total_string += s[i + j];
-            }
-
-            substrings.push_back(total_string.length());
-        }
         int greatest = 0;
+    int s_len = s.length();
+    for (int i = 0; i < s_len; i++) {
+        string total_string = "";
 
-        for (int i = 0; i < substrings.size(); i++) {
-            if (substrings[i] > greatest) {
-                greatest = substrings[i];
+        for (int j = 0; j < s_len - i; j++) {
+            if (total_string.find(s[i + j]) != string::npos) {
+                break;
             }
+
+            total_string += s[i + j];
         }
 
-        return greatest;
+        int str_len = total_string.length();
+
+        if (str_len > greatest) {
+            greatest = str_len;
+        }
+    }
+    return greatest;
     }
 };
