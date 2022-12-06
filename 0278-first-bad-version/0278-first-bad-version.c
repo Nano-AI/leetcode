7974b3640f58c7384bacd1a59c56ecc04f43e96c
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-05 19:24:15 -0800

    Time: 0 ms (100.00%), Space: 5.4 MB (98.78%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.c
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int l = 1;
+    int r = n;
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (isBadVersion(m)) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+}
