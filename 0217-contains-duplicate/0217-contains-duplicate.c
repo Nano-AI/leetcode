c282cb8dc089d181af57a153c7a8f603316e1690
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-05 19:22:56 -0800

    Time: 238 ms (34.34%), Space: 13 MB (49.60%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -2,6 +2,9 @@ int cmp(const void *a, const void *b) {
     return (*(int*) a - *(int*)b);
 }
 bool containsDuplicate(int* nums, int numsSize){
+    if (numsSize == 1) {
+        return false;
+    }
     qsort(nums, numsSize, sizeof(int), cmp);
     for (int i = 0; i < numsSize - 1; i++) {
         if (nums[i] == nums[i + 1]) {
