3465a7214e92996273cd4fa1e552e9a8a7c702f7
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-05 19:35:53 -0800

    Time: 0 ms (100.00%), Space: 5.9 MB (76.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if (x < -2147483648 || x > 2147483647) {
+        return 0;
+    }
+    long out = 0;
+    long iterate = x;
+    while (iterate != 0) {
+        out *= 10;
+        out += iterate % 10;
+        iterate /= 10;
+    }
+    if (out < -2147483648 || out > 2147483647) {
+        return 0;
+    }
+    return out;
+    }
+};
