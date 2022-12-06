67e789a63a62d48dfbcda3af32dbf54db46f05a6
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-05 19:16:30 -0800

    Time: 9 ms (35.45%), Space: 5.9 MB (79.13%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,13 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int l = 0;
+    int h = numsSize - 1;
+    while (l <= h) {
+        int m = l+(h-l)/2;
+        if (nums[m] < target) {
+            l = m+1;
+        } else {
+            h = m-1;
+        }
+    }
+    return l;
+}
