a18ebbeb7e9629dc39c7be70b679366907fbfe10
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-11 10:59:01 -0800

    Time: 5 ms (73.26%), Space: 10.8 MB (32.83%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,15 +1,13 @@
-    ListNode *next;                                        /**
-    ListNode() : val(0), next(nullptr) {}                   * Definition for singly-linked list.
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-/*
-
-
-
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-// @lc code=start
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
