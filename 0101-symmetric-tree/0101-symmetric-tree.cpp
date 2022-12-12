e685a55d4b5b11b7e037925f39da00778a33acfe
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2022-12-11 20:36:14 -0800

    Time: 9 ms (52.80%), Space: 16.5 MB (18.93%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool validate(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) {
+            return true;
+        }
+        if (left == nullptr || right == nullptr) {
+            return false;
+        }
+        return (left->val == right->val) && validate(left->left, right->right) && validate(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return validate(root->left, root->right);
+    }
+};
